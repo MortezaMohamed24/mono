fc901318bfa95c1f2f80a78a1e4c3e210947ff52
Morteza <mortezmohamed24@gmail.com>
2022-04-21 11:24:17 +0300

    Update './common/loggingSerializer'
    Convert it into a standalon package.

--- common/logingSerializer/index.ts
+++
@@ -1,9 +0,0 @@
-import serialize from "./serializers";
-import typeNameOf from "./typeNameOf";
-
-export {
-  serialize,
-  typeNameOf,
-}
-
-export default serialize;
--- common/logingSerializer/typeNameOf.ts
+++
@@ -1,9 +0,0 @@
-const toString = window.Object.prototype.toString;
-
-
-const typeNameOf = (v: unknown) => (
-  toString.call(v).slice(8, -1)
-);
-
-
-export default typeNameOf;
